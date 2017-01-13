728a46a247f4b0b739c9c0c87bb67eeb9135dd45
TShvets <tatyana.shvets@itechart-group.com>
2017-01-13 16:48:40 +0300

    move-map-section-to-Contact-page

--- Pricepy/static/app/controllers/contactController.js
+++ Pricepy/static/app/controllers/contactController.js
@@ -1,5 +1,15 @@
 app.controller('contactController', function contactController($scope){
     console.log("contact controller runs");
     $scope.$parent.path = "/contact";
-}
-)
+
+    var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+        console.log("init map called");
+    };
+
+    initMap();
+})
