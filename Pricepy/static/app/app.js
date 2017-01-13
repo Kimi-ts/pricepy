0d9391afbd25a1377c472959710c5a69d8466775
TShvets <tatyana.shvets@itechart-group.com>
2017-01-13 14:45:24 +0300

    mix-MVC-and-Angular-routes-in-header-menu

--- Pricepy/static/app/app.js
+++ Pricepy/static/app/app.js
@@ -1,1 +1,17 @@
-// var myApp = angular.module('myApp', ['ngRoute']);
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "static/app/templates/main.htm"
+    })
+    .when("/red", {
+        templateUrl: "static/app/templates/menu.htm"
+    })
+    .when("/green", {
+        templateUrl: "static/app/templates/menu.htm"
+    })
+    .when("/blue", {
+        templateUrl: "static/app/templates/main.htm"
+    });
+});
