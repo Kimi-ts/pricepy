350eaec1e363646b9c63a1601a537b916f18e637
TShvets <tatyana.shvets@itechart-group.com>
2017-01-16 15:01:19 +0300

    add-contact-form-validation

---
+++ Pricepy/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using Pricepy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pricepy.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public bool Post(FormData data)
+        {
+            if (data == null)
+            {
+                //To Do:
+                //Add DB logic here
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Pricepy/static/app/controllers/contactController.js
+++ Pricepy/static/app/controllers/contactController.js
@@ -1,15 +1,26 @@
-app.controller('contactController', function contactController($scope){
+app.controller('contactController', function contactController($scope, $http){
     console.log("contact controller runs");
     $scope.$parent.path = "/contact";
-
-    var map;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8
+    $scope.isSucceeded = false;
+    $scope.isError = false;
+    $scope.submit = function(){
+        if ($scope.messagesForm.$invalid){
+            return;
+        }
+        $http({
+            method: 'POST',
+            url: '/api/Messages',
+            data: $scope.form,
+        }).then(function successCallback(response) {
+            console.log(response);
+            if (response.data){
+                $scope.isSucceeded = true;
+            }
+            else{
+                $scope.isError = true;
+            }
+        }, function errorCallback(response) {
+             $scope.isError = true;
         });
-        console.log("init map called");
     };
-
-    initMap();
 })
