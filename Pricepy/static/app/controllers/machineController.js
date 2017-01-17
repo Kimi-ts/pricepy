119037463705157a3a9f723a240e94c544987028
TShvets <tatyana.shvets@itechart-group.com>
2017-01-17 16:04:21 +0300

    add-gallery-clickable-images

--- Pricepy/static/app/controllers/machineController.js
+++ Pricepy/static/app/controllers/machineController.js
@@ -3,5 +3,17 @@ app.controller('machineController', function machineController($scope, $routePar
     //to make section active in main menu
     $scope.$parent.path = "/machines";
     console.log($routeParams);
-}
-)
+
+    // TO DO
+    // create mechanism to use first item src by default
+    $scope.activeSrc = "static/content/2.jpg";
+
+    // TO DO
+    // retrieve images from backend 
+
+    $scope.secondaryImages = [ "static/content/1.jpg", "static/content/2.jpg", "static/content/3.jpeg"];
+
+    $scope.setActive = function(activeSrc){
+        $scope.activeSrc = activeSrc;
+    }
+})
