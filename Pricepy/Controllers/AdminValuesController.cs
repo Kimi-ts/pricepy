1b223d8a89bc0b881bc9761493bfa5904ce69412
TShvets <tatyana.shvets@itechart-group.com>
2017-01-23 17:25:21 +0300

    add-admin-panel

---
+++ Pricepy/Controllers/AdminValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Pricepy.Controllers
+{
+    public class AdminValuesController : ApiController
+    {
+        public object Get()
+        {
+            string currentDir = Environment.CurrentDirectory;
+            var path = System.Web.HttpContext.Current.Request.MapPath("~\\Content\\adminConfig.json");
+            string allText = System.IO.File.ReadAllText(path);
+
+            object jsonObject = JsonConvert.DeserializeObject(allText);
+            return jsonObject;
+        }
+    }
+}
---
+++ Pricepy/static/admin/services/getDataService.js
@@ -0,0 +1,12 @@
+﻿adminApp.service("getData", function($http){
+    var promise = $http({
+         method: 'Get',
+         url: '/api/adminValues',
+    }).then(function successCallback(response) {
+        return response.data;
+    }, function errorCallback(response) {
+        console.log("error");
+    });
+
+    return promise;
+})
