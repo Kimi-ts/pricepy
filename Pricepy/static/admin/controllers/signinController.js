436fb0cd37062fc9c928b46d8f5cb0c63ca6b082
TShvets <tatyana.shvets@itechart-group.com>
2017-01-25 14:49:09 +0300

    add-frontend-signin-form-including-error

--- Pricepy/static/admin/controllers/signinController.js
+++ Pricepy/static/admin/controllers/signinController.js
@@ -1,31 +1,34 @@
 ﻿adminApp.controller('signinController', function signinController($scope, $http, getData){
     console.log("sign in controller runs");
     $scope.$parent.path = "/sign in";
+    
+    $scope.isError = false;
 
     getData.then(function(data){
         $scope.data = data.signin;
         console.log($scope.data);
 
-        $scope.isSucceeded = false;
-        $scope.isError = false;
         $scope.submit = function(){
             if ($scope.messagesForm.$invalid){
                 return;
             }
             $http({
                 method: 'POST',
-                url: '/api/Messages',
+                url: '/api/AdminValues',
                 data: $scope.form,
             }).then(function successCallback(response) {
                 console.log(response);
                 if (response.data){
-                    $scope.isSucceeded = true;
+                    //$scope.isSucceeded = true;
+                    console.log("true")
                 }
                 else{
                     $scope.isError = true;
                 }
             }, function errorCallback(response) {
-                $scope.isError = true;
+                //$scope.isError = true;
+                console.log("error occured on our server");
+                console.log(response);
             });
         };
     })
