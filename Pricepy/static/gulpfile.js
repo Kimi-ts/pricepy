88b84c379aad60e3c72e9e91e6aa10c1161f221b
TShvets <tatyana.shvets@itechart-group.com>
2017-04-06 12:28:53 +0300

    add-gulp-config

---
+++ Pricepy/static/gulpfile.js
@@ -0,0 +1,19 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+
+gulp.task('minify-css', function () {
+    return gulp.src('styles.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(rename("styles.min.css"))
+        .pipe(gulp.dest(''))
+});
