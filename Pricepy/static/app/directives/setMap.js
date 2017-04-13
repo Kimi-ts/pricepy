7576648231f5d5a95dfef6af5c8aff89ab6dd0fa
TShvets <tatyana.shvets@itechart-group.com>
2017-04-13 14:35:15 +0300

    add-title-icon-fix-vk-page-description

--- Pricepy/static/app/directives/setMap.js
+++ Pricepy/static/app/directives/setMap.js
@@ -4,10 +4,14 @@ app.directive("setMap", function($timeout){
         link: function(scope, element, attrs) {
             var map;
             function initMap() {
-                var myLatLng = {lat: 53.7782142, lng: 27.5949404};
+                //old address
+                //var myLatLng = {lat: 53.7782142, lng: 27.5949404};
+
+                //new address Minsk Vulica Karzhaneyewskaha 16b
+                var myLatLng = {lat: 53.841885, lng: 27.524684};
 
                 var map = new google.maps.Map(element[0], {
-                    zoom: 10,
+                    zoom: 14,
                     center: myLatLng
                 });
 
