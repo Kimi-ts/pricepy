5646a96779ebdf509692c6c611a8e7d87c49e20a
TShvets <tatyana.shvets@itechart-group.com>
2017-09-22 14:43:12 +0300

    Revert "Revert "now-admin-can-add-and-remove-prices""
    This reverts commit c21359d3a7f868e281782d51493baae0acdec0b2.

---
+++ Pricepy/static/admin/services/calendarService.js
@@ -0,0 +1,17 @@
+adminApp.service('calendarService', function(){
+    return {
+        getFirstDayOfMonth: function(dateStr){
+            var dd = new Date(dateStr);
+            dd.setDate(1);
+            return dd.getDay();
+        },
+
+        getLastDayOfMonth: function(dateStr){
+            var dd = new Date(dateStr);
+
+            //0 as day means previous month last day
+            var cc = new Date(dd.getFullYear(), dd.getMonth()+1, 0);
+            return cc.getDay();
+        }
+    }
+});
